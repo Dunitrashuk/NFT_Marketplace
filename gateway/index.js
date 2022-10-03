42767e5736ae3b9bd65e0966398ecc9e33039ed4
Dunitrashuk <56090992+Dunitrashuk@users.noreply.github.com>
2022-10-04 01:18:30 +0300

    project initialization

---
+++ gateway/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const proxy = require('express-http-proxy');
+
+app.use(cors());
+app.use(express.json());
+app.use('/user', proxy('http://localhost:8001'))
+app.use('/products', proxy('http://localhost:8002'))
+
+app.listen(8000, () => {
+    console.log('Gateway is Listening to port 8000')
+})
